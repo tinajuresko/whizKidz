3b57b4ce0626e7336239378a6ca99b0b37a55782
tinajuresko <tina.juresko16@gmail.com>
2024-12-31 15:54:04 +0100

    Initial Commit

---
+++ whizKidz/whizKidzApp.swift
@@ -0,0 +1,17 @@
+//
+//  whizKidzApp.swift
+//  whizKidz
+//
+//  Created by Tina Jureško on 31.12.2024..
+//
+
+import SwiftUI
+
+@main
+struct whizKidzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ whizKidzTests/whizKidzTests.swift
@@ -0,0 +1,17 @@
+//
+//  whizKidzTests.swift
+//  whizKidzTests
+//
+//  Created by Tina Jureško on 31.12.2024..
+//
+
+import Testing
+@testable import whizKidz
+
+struct whizKidzTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
